4b60134beabc5a3b38e5cc1633216fd0e8fb1e7e
Daniel Kaye <daniel.n.kaye@gmail.com>
2021-02-08 08:22:20 -0700

    adding regions
    started adding //#region & //#endregion for easier VS folding code navigation

--- rebusCityLocal.js
+++ rebusCityLocal.js
@@ -1,3 +1,5 @@
+//#region ABOUT
+
 ////////////////////////////////////////////////////////////////////////////////
 // About ///////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
@@ -14,25 +16,27 @@
 //
 // What is a rebus puzzle?
 //  A rebus puzzle is a collections of letters, symbols, and images that add up
-//  to a known word or phrase, sometimes relating to aparticulat topic or
-//  category, for example 'coding'.
+//  to a known word or phrase, sometimes relating to a particulat topic or
+//  category, for example 'coding' or 'snowboarding'
 //
 // What can you do on this site?
 //  You can browser and attempt to solve rebus puzzles, while thes site lets you
-//  know if you've evtered a correct or incorrect answer, how many you've solved
-//  and other random statistics.
+//  know if you've entered a correct or incorrect answer, how many you've solved,
+//  privdes hints, and other random statistics.
 //  
-// My Formatting Rules:
+// My Coding Style Guide:
 //  comments after OR above the line they're about
 //  no code longer than 80 charecters ('cept paths & comments)
 //  no empty lines within a function
 //  one empty line between functions
 //  three empty lines before new section
 
+//#endregion ABOUT
 
+//#region TODO
 
 ////////////////////////////////////////////////////////////////////////////////
-// Todo ////////////////////////////////////////////////////////////////////////
+// TODO ////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 // RECENTLY ADDED FEATURES ////////////////////
 //
@@ -138,7 +142,9 @@
 //  -add to github and share with greg
 //  -add js unit tests somehow
 
+// #endregion TODO
 
+//#region GLOBAL VARIABLES
 
 ////////////////////////////////////////////////////////////////////////////////
 // Global Variables ////////////////////////////////////////////////////////////
@@ -249,7 +255,9 @@ const footerHeight = 20;  // height of DOM footer element
 let totalScoreP; // <p> dispalying current  number of rebuses solved
 let categoryScoreP; // <p> displaying current number of rebuses for current category solved
 
+//#endregion GLOBAL VARIABLES
 
+//#region PRELOAD
 
 ////////////////////////////////////////////////////////////////////////////////
 // Preload /////////////////////////////////////////////////////////////////////
@@ -266,7 +274,9 @@ function preload(){
   rebusRankDataTable = loadTable(rebusRankCsvPath, 'csv', 'header');
 }
 
+//#endregion PRELOAD
 
+//#region SETUP
 
 ////////////////////////////////////////////////////////////////////////////////
 // Setup ///////////////////////////////////////////////////////////////////////
@@ -305,7 +315,9 @@ function setup(){
   setInterval(calcScrollRate, 100);
 }
 
+//#endregion SETUP
 
+//#region DRAW LOOP
 
 ////////////////////////////////////////////////////////////////////////////////
 // Draw Loop ///////////////////////////////////////////////////////////////////
@@ -330,6 +342,7 @@ function draw(){
   print('sv : ' + round(swipeVelocity) + ', svr: ' + round(swipeVelocityRemaining));
 }
 
+//#endregion DRAW LOOP
 
 
 ////////////////////////////////////////////////////////////////////////////////
