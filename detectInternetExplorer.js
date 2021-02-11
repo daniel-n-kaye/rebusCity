fe886160c997802b92a9b0c771c9cbcf466ef4da
Daniel Kaye <daniel.n.kaye@gmail.com>
2021-02-10 17:36:55 -0700

    added Internet Explorer warning
    - added it as a separate script, which loads before the p5.js library, because the p5.js library will break on IE!

---
+++ detectInternetExplorer.js
@@ -0,0 +1,10 @@
+﻿if (detectInternetExplorer())
+    alert("Warning: Your current browser in not supported, and RebusCity.com may not display properly!");
+
+/** Returns true if the site is being viewed through Internet Explorer
+* @returns {bool} */
+function detectInternetExplorer() {
+    user_agent = navigator.userAgent;
+    var isBrowserIE = user_agent.indexOf("MSIE ") > -1 || user_agent.indexOf("Trident/") > -1;
+    return isBrowserIE;
+}
