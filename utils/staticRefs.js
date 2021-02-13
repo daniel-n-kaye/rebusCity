ccaed048a832485a1897a9cc31500161367e141e
Daniel Kaye <daniel.n.kaye@gmail.com>
2021-02-13 13:12:54 -0700

    commented isFunction function

--- utils/staticRefs.js
+++ utils/staticRefs.js
@@ -8,6 +8,12 @@ staticRefs.initializeCanvas = function() {
     canvas.parent('container'); // sets parent of canvas as container div
 }
 
-staticRefs.isFunction = function(functionToCheck) {
-    return functionToCheck && {}.toString.call(functionToCheck) === '[object Function]';
+/**
+ *  returns true if the given argument is a function
+ *  got from here: https://stackoverflow.com/questions/5999998/check-if-a-variable-is-of-function-type
+ * @param {any} functionToCheck
+ * @returns {boolean}
+ */
+staticRefs.isFunction = function(thingToCheck) {
+    return thingToCheck && {}.toString.call(thingToCheck) === '[object Function]';
 }
